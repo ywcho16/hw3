c91682d838db39377185e19edc0b51698c36e33f
ywcho16 <163232974+ywcho16@users.noreply.github.com>
2024-03-20 14:25:52 +0900

    Add files via upload

--- cal.cpp
+++ cal.cpp
@@ -1,78 +1,55 @@
 #include <iostream>
 using namespace std;
+const float PI = 3.141592;
 void main() {
 	float c, C, dt, t, num, anal;
-	//differentiation
-	cout << "c, C =";
+	//// f(x) = C*sin(c*t)
+	//// differentiation
+	//cout << " c C ? = ";
+	//cin >> c >> C;
+	//cout << " t dt ? = ";
+	//cin >> t >> dt;
+	//anal = c * C * cos(c * PI / t);
+	//num = C * (sin(c * (PI / t + PI / dt)) - sin(c * PI / t)) / (PI / dt);
+	//cout << " num " << num << " anal " << anal << endl;
+	//cout << " error = " << (anal - num) / anal * 100 << "%\n";
+
+	//// integration
+	//float a, b;
+	//int N;
+	//cout << " a b N = ";
+	//cin >> a >> b >> N;
+	//anal = -C * (cos(c * PI / b) - cos(c * PI / a)) / c;
+	//dt = ((PI / b) - (PI / a)) / N;
+	//num = 0;
+	//for (int i = 0; i < N; i++) {
+	//	num += C * sin(c * (PI / a + dt * i)) * dt;
+	//}
+	//cout << " num " << num << " anal " << anal << endl;
+	//cout << " error = " << (num - anal) / anal * 100 << "%\n";
+
+	// f(x) = C*cos(c*t)
+	// differentiation
+	cout << " c C ? = ";
 	cin >> c >> C;
-	cout << "t, dt =";
+	cout << " t dt ? = ";
 	cin >> t >> dt;
-	anal = -c * C * exp(-c * t);
-	num = C * (exp(-c * (t + dt)) - exp(-c * t)) / dt;
-	cout << "num" << num << "anal" << anal << endl;
-	cout << "error = " << (num - anal) / anal * 100 << "%\n";
+	anal = -c * C * sin(c * PI / t);
+	num = C * (cos(c * (PI / t + PI / dt)) - cos(c * PI / t)) / (PI / dt);
+	cout << " num " << num << " anal " << anal << endl;
+	cout << " error = " << (anal - num) / anal * 100 << "%\n";
+
 	// integration
 	float a, b;
 	int N;
 	cout << " a b N = ";
 	cin >> a >> b >> N;
-	anal = -C * (exp(-c * b) - exp(-c * a)) / c;
-	dt = (b - a) / N;
+	anal = C * (sin(c * PI / b) - sin(c * PI / a)) / c;
+	dt = ((PI / b) - (PI / a)) / N;
 	num = 0;
 	for (int i = 0; i < N; i++) {
-		num += C * exp(-c * (a + dt * i)) * dt;
+		num += C * cos(c * (PI / a + dt * i)) * dt;
 	}
 	cout << " num " << num << " anal " << anal << endl;
 	cout << " error = " << (num - anal) / anal * 100 << "%\n";
-
-	//Csin(ct)
-	//differentiation
-	cout << "c, C =";
-	cin >> c >> C;
-	cout << "t, dt =";
-	cin >> t >> dt;
-	anal = c * C * cos(c * t);
-	num = C * (sin(c * (t + dt)) - sin(c * t)) / dt;
-	cout << "num" << num << "anal" << anal << endl;
-	cout << "error = " << (num - anal) / anal * 100 << "%\n";
-	// integration
-	float a, b;
-	int N;
-	cout << " a b N = ";
-	cin >> a >> b >> N;
-	anal = -C * (cos(c * b) - cos(-c * a)) / c;
-	dt = (b - a) / N;
-	num = 0;
-	for (int i = 0; i < N; i++) {
-		num += C * sin(-c * (a + dt * i)) * dt;
-	}
-	cout << " num " << num << " anal " << anal << endl;
-	cout << " error = " << (num - anal) / anal * 100 << "%\n";
-
-	//Ccos(ct)
-	//differentiation
-	cout << "c, C =";
-	cin >> c >> C;
-	cout << "t, dt =";
-	cin >> t >> dt;
-	anal = -c * C * sin(c * t);
-	num = C * (cos(c * (t + dt)) - cos(c * t)) / dt;
-	cout << "num" << num << "anal" << anal << endl;
-	cout << "error = " << (num - anal) / anal * 100 << "%\n";
-	// integration
-	float a, b;
-	int N;
-	cout << " a b N = ";
-	cin >> a >> b >> N;
-	anal = C * (sin(c * b) - sin(-c * a)) / c;
-	dt = (b - a) / N;
-	num = 0;
-	for (int i = 0; i < N; i++) {
-		num += C * cos(-c * (a + dt * i)) * dt;
-	}
-	cout << " num " << num << " anal " << anal << endl;
-	cout << " error = " << (num - anal) / anal * 100 << "%\n";
-
-	//Cln(ct)
-	
 }
